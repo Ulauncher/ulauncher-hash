7d8504b097c7750712c0485fcd12ae7163157ffa
Albin <mail@albinlarsson.com>
2018-09-12 15:43:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ulauncher-hash2
+[Ulauncher](https://ulauncher.io/) extension to generate hashes with md5, sha1 and sha2 (sha224, sha256, sha384 and sha512).
+
+## Usage
+* Install in Ulauncher using this url (https://github.com/friday/ulauncher-hash2)
+* type `hash ` to show all supported hashes, or a specific algorithm like `md5` or `sha256` followed by a space and the string you want to hash. Ex `hash 123`, `md5 hello` or `sha256 foo bar` (any following space will be interpreted as part of the string).
+
+## Credits
+* The [icon](https://www.flaticon.com/free-icon/encryption_418404) was created by [freepik](https://www.flaticon.com/authors/freepik) from [www.flaticon.com](https://www.flaticon.com/).
+* [Bruno Paz](https://github.com/brpaz) for the original [ulauncher-hash](https://github.com/brpaz/ulauncher-hash) (please merge my [PR](https://github.com/brpaz/ulauncher-hash/pull/4)).
---
+++ main.py
@@ -0,0 +1,35 @@
+import hashlib
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+
+class Hash2Extension(Extension):
+    def __init__(self):
+        super(Hash2Extension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        items = []
+        argument = (event.get_argument() or '').encode('utf-8')
+        keyword = event.get_keyword()
+
+        # Find the keyword id using the keyword (since the keyword can be changed by users)
+        for kwId, kw in extension.preferences.iteritems():
+            if kw == keyword:
+                keywordId = kwId
+
+        # Show the algorithm specified as keyword, or all if the keyword was "hash"
+        algos = hashlib.algorithms if keywordId == 'hash' else [keywordId]
+
+        for algo in algos:
+            hash = getattr(hashlib, algo)(argument).hexdigest()
+            items.append(ExtensionResultItem(icon='icon.svg', name=hash, description=algo, on_enter=CopyToClipboardAction(hash)))
+
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    Hash2Extension().run()
